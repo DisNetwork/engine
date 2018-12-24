98dfc53d8f84b77af6b5094b8927a135f5ba0296
iHDeveloper <hamzadevelop@gmail.com>
2018-12-24 22:57:34 +0300

    Add gateway version and encoding

--- src/discord.ts
+++ src/discord.ts
@@ -2,6 +2,9 @@ export class DiscordAPI {
     public static readonly ENDPOINT: string = "https://discordapp.com/api/";
     public static readonly GET_GATEWAY_BOT: string = "gateway/bot";
 
+    public static readonly GATEWAY_VERSION: number = 6;
+    public static readonly GATEWAY_COMPRESS: string = "json";
+
     public static getEndpoint(route: string) {
         return this.ENDPOINT + route;
     }
