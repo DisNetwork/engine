a95a9f6740bf9970d7174c372b81cba7262ae0e9
iHDeveloper <hamzadevelop@gmail.com>
2018-12-26 15:46:26 +0300

    Fix the database returns

--- src/cloud/database.ts
+++ src/cloud/database.ts
@@ -1,19 +1,19 @@
-import { SnowFlake } from '@disnetwork/core';
+import { SnowFlake, Guild, Channel, User } from '@disnetwork/core';
 
 export namespace CloudDatabase {
 
     export interface Guilds {
-        get(id: SnowFlake): any;
+        get(id: SnowFlake): Guild;
         has(id: SnowFlake): boolean;
     }
 
     export interface Channels {
-        get(id: SnowFlake): any;
+        get(id: SnowFlake): Channel;
         has(id: SnowFlake): boolean;
     }
 
     export interface Users {
-        get(id: SnowFlake): any;
+        get(id: SnowFlake): User;
         has(id: SnowFlake): boolean;
     }
 
