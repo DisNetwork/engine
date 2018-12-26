1595493226d53b936f248eefb75260524dcb3443
iHDeveloper <hamzadevelop@gmail.com>
2018-12-26 14:52:38 +0300

    Add cloud to the executor to use it in the future

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { CloudEngine } from './cloud';
 import { LoggerLevel, Logger } from './logger';
 import { Manager } from './manager';
 import { GatewayManager } from './gateway';
@@ -7,17 +8,30 @@ export class BotExecutor {
     private token: string;
     private logger: Logger;
     private manager: Manager | GatewayManager | undefined;
+    private _cloud: CloudEngine | undefined;
 
-    public constructor(token: string, type: BotExecuteType, logLevel: LoggerLevel) {
-        this.token = token;
-        this.logger = new Logger(logLevel, "[DisNetwork] [LOG] ");
+    public constructor(
+        token: string,
+        type: BotExecuteType,
+        logLevel: LoggerLevel,
+        cloud?: CloudEngine
+    ) {
         if (BotExecutor.instance) {
             throw new Error("More then one instance for the bot executor");
+        }
+        this.token = token;
+        this.logger = new Logger(logLevel, "[DisNetwork] [LOG] ");
+        if (cloud) {
+            this._cloud = cloud;
         }
         if (type === BotExecuteType.GATEWAY) {
             this.manager = new GatewayManager(this.token, this.logger);
             this.manager.execute();
         }
+    }
+
+    get cloud(): CloudEngine | undefined {
+        return this._cloud;
     }
 
 }
