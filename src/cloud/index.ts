aabfd9e585aa643f6e5693d51a7f40c1855776c7
iHDeveloper <hamzadevelop@gmail.com>
2018-12-26 14:36:10 +0300

    Export from database.ts

--- src/cloud/index.ts
+++ src/cloud/index.ts
@@ -1,5 +1,12 @@
+import { CloudDatabase } from "./database";
+
 export interface CloudEngine {
     name: string;
 
-    // TODO cloud database
+    guilds: CloudDatabase.Guilds;
+    channels: CloudDatabase.Channels;
+    users: CloudDatabase.Users;
 }
+
+// Export everything from database
+export * from "./database";
