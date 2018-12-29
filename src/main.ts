7de6e8bfa49fce0de09be5f4480ab14d04c23b70
iHDeveloper <hamzadevelop@gmail.com>
2018-12-30 01:39:35 +0300

    The main script that will start the engine

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { textSync } from 'figlet';
+console.log(textSync('DisNetwork', {
+    font: 'Standard',
+    horizontalLayout: 'fitted',
+    verticalLayout: 'fitted'
+}));
+console.log("-------------------------------");
+console.log("DisNetwork Engine© | https://github.com/DisNetwork/engine");
