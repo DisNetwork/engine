99777d18e746954d1f3bb18a5d397b565a8ae49e
iHDeveloper <hamzadevelop@gmail.com>
2018-12-30 12:13:33 +0300

    Start the http server on running the engine

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { textSync } from 'figlet';
+import { HTTPManager } from './http';
+import ora from 'ora';
+const cliSpinners = require('cli-spinners');
 console.log(textSync('DisNetwork', {
     font: 'Standard',
     horizontalLayout: 'fitted',
@@ -6,3 +9,19 @@ console.log(textSync('DisNetwork', {
 }));
 console.log("-------------------------------");
 console.log("DisNetwork Engine© | https://github.com/DisNetwork/engine");
+
+start();
+
+async function start() {
+    // TODO Start the executor manager
+    // Start the http server
+    const httpServer: HTTPManager = new HTTPManager();
+    const httpSpinner = ora({
+        spinner: cliSpinners.dots12,
+        color: 'yellow',
+        text: 'Starting HTTP Server...'
+    }).start();
+    await httpServer.start();
+    httpSpinner.succeed();
+    httpSpinner.text = "HTTP Server is listening on port " + httpServer.port;
+}
